52e75c38ad268e808e8af939c0574857eeafce87
ananyapuru <ananya.purushottam@yale.edu>
2025-02-17 21:27:55 -0500

    Basic GET request and communication setup for BE + FE

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enabling cross-origin requests
 
 @app.route('/')
 def hello():
---
+++ frontend/src/app/constants.js
@@ -0,0 +1,6 @@
+// app/constants.js 
+
+export const API_ENDPOINTS = {
+    BASE_URL: 'http://localhost:5000',
+    HELLO: '/',
+  };
